6f511baf1cfe41eb78c8371b6353c8e05a303790
Ahmad20051 <99892893+Ahmad20051@users.noreply.github.com>
2022-03-17 19:00:36 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -1,72 +1,24 @@
+#importerar för att läsa in workbook och den kommer från pyxl.
 from openpyxl import load_workbook
-from openpyxl.utils import *
 
-wb = load_workbook("textspel.xlsx")
+#varablar
+wb = load_workbook("Programmering textspel.xlsx")
 ws = wb.active
-room_number = 2
-room_number_change_n = -1
-room_number_change_s = 1
-room_position_change = 1
-#Den visar hur mycket spelaren rör sig
-position = {1:'A', 2:'B', 3:'C'}
-room_position = 3
-start = True
-wsRooms = wb["Rooms"]
-wsMap = wb["map"]
+start= True
 
+#skriver man help så kommer alla commandon man kan använda.
+def help():
+    print("look,north,south,west,stop,active room")
 
-
-
-def look():
-    print(ws[F"{position[room_position]}{room_number}"].value)
-    #Printar ut
-
-
-def north():
-    global room_number
-    if room_number >= 1:
-        room_number += room_number_change_n
-        print(ws[F"{position[room_position]}{room_number}"].value)
-
-def south():
-    global room_number
-    if room_number >= 1:
-        room_number += room_number_change_s
-        print(ws[F"{position[room_position]}{room_number}"].value)
-
-
-def west():
-    global room_position
-    if room_position >= 1:
-        room_position -= room_position_change
-        print(ws[F"{position[room_position]}{room_number}"].value)
-        #Gör exakt samma sak som East def fast den subtraherar istället för addera
-
-def east():
-    global room_position
-    if room_position >= 1:
-        room_position += room_position_change
-        print(ws[F"{position[room_position]}{room_number}"].value)
-        #Jag har gjort bokstäverna till siffror.
-        #Det gör att jag kan addera eller subtrahera ett tal för att förflytta spelaren.
-
-
-
-
+#en loop som läser in en input, ifall man skriver skriver help så hjälper den dig, ifall man skríver stop så anvlutas loopen
 while start:
     reading = input("")
-    if reading == "look":
-        look()
-    if reading == 'north':
-        north()
-    if reading == "south":
-        south()
-    if reading == "west":
-        west()
-    elif reading == "stop":
-        start = False
-    if reading == 'quit':
-        start = False
+    if reading == "help":
+        help()
+    if reading == "stop":
+        start=False
+
+
 
 
 
