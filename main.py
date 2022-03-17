119fc5f67c0fce1691ec7dea7d9adff95ec85e5b
Karim1x <95087773+Karim1x@users.noreply.github.com>
2022-03-17 14:56:53 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -1,55 +1,43 @@
 from openpyxl import load_workbook
 from openpyxl.utils import *
 
-wb = load_workbook("textspel.xlsx")
+wb = load_workbook("programmering texspel.xlsx")
 ws = wb.active
-room_number = 2
+#definierar save work sheeten
+save_file = wb['Save']
+room_number = 1
 room_number_change_n = -1
 room_number_change_s = 1
-room_position_change = 1
-#Den visar hur mycket spelaren rör sig
-position = {1:'A', 2:'B', 3:'C'}
-room_position = 3
+room_position = 'C'
 start = True
 wsRooms = wb["Rooms"]
 wsMap = wb["map"]
 
-
-
+#denna sparar min position i excel dokumentet.
+wb.save("programmering texspel.xlsx")
+room_number = (int(str(save_file["A2"].value)))
 
 def look():
-    print(ws[F"{position[room_position]}{room_number}"].value)
-    #Printar ut
+    print(ws[F"{room_position}{room_number}"].value)
 
 
 def north():
     global room_number
     if room_number >= 1:
         room_number += room_number_change_n
-        print(ws[F"{position[room_position]}{room_number}"].value)
+        print(ws[F"{room_position}{room_number}"].value)
 
 def south():
     global room_number
     if room_number >= 1:
         room_number += room_number_change_s
-        print(ws[F"{position[room_position]}{room_number}"].value)
-
+        print(ws[F"{room_position}{room_number}"].value)
 
 def west():
     global room_position
-    if room_position >= 1:
-        room_position -= room_position_change
-        print(ws[F"{position[room_position]}{room_number}"].value)
-        #Gör exakt samma sak som East def fast den subtraherar istället för addera
-
-def east():
-    global room_position
-    if room_position >= 1:
-        room_position += room_position_change
-        print(ws[F"{position[room_position]}{room_number}"].value)
-        #Jag har gjort bokstäverna till siffror.
-        #Det gör att jag kan addera eller subtrahera ett tal för att förflytta spelaren.
-
+    if room_position == 1:
+        room_position -= 1
+        print(ws[F"{room_position}{room_number}"].value)
 
 
 
@@ -61,13 +49,12 @@ while start:
         north()
     if reading == "south":
         south()
-    if reading == "west":
-        west()
     elif reading == "stop":
         start = False
     if reading == 'quit':
         start = False
-
-
-
+   #Denna läser in columen där min senaste position har sparats och sätter in mig i det rum numret som sparats i columen.
+    if reading == "save":
+        save_file["A2"]=room_number
+        wb.save("programmering texspel.xlsx")
 
